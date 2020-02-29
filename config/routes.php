3bc655be10455d4f809bac793fb4d9e4d8ac588d
Aleksander Artemov <roadtoexp@gmail.com>
2020-02-29 22:56:32 +0300

    [fix] Fix route callback

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,5 @@
 
 $routes = [
     'GET /users/{user_id}/services/{service_id}/tarifs' => 'UserController@getTariffs',
-    'PUT /users/{user_id}/services/{service_id}/tarif' => 'TariffController@getTariffs'
+    'PUT /users/{user_id}/services/{service_id}/tarif' => 'ServiceController@updateTariff'
 ];
